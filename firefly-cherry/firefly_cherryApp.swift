134c26d58a3dfa008d2e9e9a9dc75a8efb61eaed
bucketfishy <bucketfishy@gmail.com>
2023-09-06 13:01:08 +0800

    Initial Commit

---
+++ firefly-cherry/firefly_cherryApp.swift
@@ -0,0 +1,20 @@
+//
+//  firefly_cherryApp.swift
+//  firefly-cherry
+//
+//  Created by Tongyu Jiang on 6/9/23.
+//
+
+import SwiftUI
+
+@main
+struct firefly_cherryApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
